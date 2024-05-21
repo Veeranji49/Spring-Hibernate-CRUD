b9aadb96072a38d1d92324724ed6f1bacb48e34f
Veeranji49 <veeranjijlkr49@gmail.com>
2024-05-21 11:05:01 +0530

    spring-hibernate-crud configuration

--- src/main/java/com/spring/Spring_Hibernate_CRUD/Entity/Employee.java
+++ src/main/java/com/spring/Spring_Hibernate_CRUD/Entity/Employee.java
@@ -2,8 +2,8 @@ package com.spring.Spring_Hibernate_CRUD.Entity;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
+//import javax.persistence.GeneratedValue;
+//import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
